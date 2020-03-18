507b817ac0dcb96d79ae9327ac28bc543ddb80d4
chenhsi <sungchenhsi@gmail.com>
2020-03-18 23:44:52 +0000

    peak in mountain

---
+++ peakMountain.py
@@ -0,0 +1,10 @@
+def peakIndexInMountainArray(A):
+    l, r = 0, len(A) - 1
+    while l < r:
+        m = (l + r) / 2
+        if A[m] < A[m + 1]:
+            l = m + 1
+        else:
+            r = m
+    return l
+

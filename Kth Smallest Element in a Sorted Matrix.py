d5327a894cbe750184ccf3082a6c5fa15d458f00
chenhsi <sungchenhsi@gmail.com>
2020-05-03 20:52:27 +0100

    mat smallest element in a sirted matrix

---
+++ Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,26 @@
+import heapq
+def kthSmallest( matrix, k):
+    heap, res = [(row[0], r, 0) 
+                for r, row in enumerate(matrix) 
+                if row], 0
+    heapq.heapify(heap)
+    for k in range(1, k + 1):
+        res, row, col = heapq.heappop(heap)
+        if col + 1 < len(matrix[row]):
+            heapq.heappush(heap, (matrix[row][col + 1], row, col + 1))
+    return res
+
+def kthSmallest2( matrix, k):
+    heap, res, n = [(matrix[0][0], 0, 0)], 0, len(matrix)
+    for k in range(1, k + 1):
+        res, row, col = heapq.heappop(heap)
+        if not row and col < n - 1:
+            heapq.heappush(heap, (matrix[row][col + 1], row, col + 1))
+        if row < n - 1:
+            heapq.heappush(heap, (matrix[row + 1][col], row + 1, col))
+    return res
+
+matrix = [[1,2],[1,3]]
+k = 2
+res = kthSmallest2( matrix, k)
+print(res)

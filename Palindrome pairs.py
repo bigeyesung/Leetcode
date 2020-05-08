0203a9a97093e6ac000b9a43bea7101b704efa3a
chenhsi <sungchenhsi@gmail.com>
2020-05-08 23:22:28 +0100

    update code change for palincrome pairs selection

--- Palindrome pairs.py
+++ Palindrome pairs.py
@@ -7,6 +7,8 @@ def palindromePairs(words):
         for j in range(len(words[i])+1):
             tmp1 = words[i][:j]
             tmp2 = words[i][j:]
+            print(tmp1)
+            print(tmp2)
             if tmp1[::-1] in wordict and wordict[tmp1[::-1]]!=i and tmp2 == tmp2[::-1]:
                 res.append([i,wordict[tmp1[::-1]]])
             if j!=0 and tmp2[::-1] in wordict and wordict[tmp2[::-1]]!=i and tmp1 == tmp1[::-1]:

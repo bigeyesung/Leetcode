5d91cfe1860930bbdbf61735648b6f0a3eddbe3e
chenhsi <sungchenhsi@gmail.com>
2020-05-31 23:18:33 +0100

    shoartest path

---
+++ 1091. Shortest Path in Binary Matrix.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def shortestPathBinaryMatrix(self, grid):
+        seen = set()
+        q= deque()
+        q.append((0,0,0))
+        n = len(grid)-1
+        while q:
+            xx,yy,step = q.popleft()
+            if xx == n and yy ==n:
+                return step
+            if (xx,yy) in seen:
+                continue
+            seen.add((xx,yy))
+
+            for dx,dy in [[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1]]:
+                if 0<=xx+dx<=n and 0<=yy+dy<=n and grid[xx+dx][yy+dy]==0:
+                    q.append((xx+dx,yy+dy,step+1))
+
+grid = [[0,1],[1,0]]
+sol = Solution()
+ret = sol.shortestPathBinaryMatrix(grid)
+print(sol+1)
+
+        

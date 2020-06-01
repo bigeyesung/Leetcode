0735745bcbc238faf373870daa471deace4536b5
chenhsi <sungchenhsi@gmail.com>
2020-06-01 22:43:54 +0100

    add corner case

--- 1091. Shortest Path in Binary Matrix.py
+++ 1091. Shortest Path in Binary Matrix.py
@@ -3,8 +3,11 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid):
         seen = set()
         q= deque()
-        q.append((0,0,0))
         n = len(grid)-1
+
+        if grid[0][0] or grid[n][n]:
+            return -1
+        q.append((0,0,0))
         while q:
             xx,yy,step = q.popleft()
             if xx == n and yy ==n:
@@ -16,6 +19,7 @@ class Solution:
             for dx,dy in [[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1]]:
                 if 0<=xx+dx<=n and 0<=yy+dy<=n and grid[xx+dx][yy+dy]==0:
                     q.append((xx+dx,yy+dy,step+1))
+        return -1
 
 grid = [[0,1],[1,0]]
 sol = Solution()

d92fe3375d0e010b015007527395205d20bbf79a
chenhsi <sungchenhsi@gmail.com>
2020-08-18 21:39:26 +0100

    code update

--- 568. Maximum Vacation Days.py
+++ 568. Maximum Vacation Days.py
@@ -12,8 +12,26 @@ def maxVacationDays(flights, days):
     return max(dp)
 
 
+def maxVacationDays2(flights, days):
+    """
+    :type flights: List[List[int]]
+    :type days: List[List[int]]
+    :rtype: int
+    """
+    N, K = len(days), len(days[0])
+    dp = [0] + [-1] * (N - 1)
+    for w in range(K):
+        ndp = [x for x in dp]
+        for sc in range(N):
+            if dp[sc] < 0: continue
+            for tc in range(N):
+                if sc == tc or flights[sc][tc]:
+                    ndp[tc] = max(ndp[tc], dp[sc] + days[tc][w])
+        dp = ndp
+    return max(dp)
+
 flights = [[0,0,0],[0,0,0],[0,0,0]]
 days = [[1,1,1],[7,7,7],[7,7,7]]
 
-ret = maxVacationDays(flights, days)
+ret = maxVacationDays2(flights, days)
 print(ret)

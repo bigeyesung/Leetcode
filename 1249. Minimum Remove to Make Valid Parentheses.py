7b3c62652a143ea2db0c43c5937ee679b966bcb4
chenhsi <sungchenhsi@gmail.com>
2020-11-03 22:19:26 +0800

    code update for remove minumum parenteheses

--- 1249. Minimum Remove to Make Valid Parentheses.py
+++ 1249. Minimum Remove to Make Valid Parentheses.py
@@ -1,6 +1,26 @@
-# iterate string
-# if c is "(": push 
-# if c is ")":pop
-
+# iterate char in a string
+# save in stack and temp var cur
+# if c is "(": push in stack
+# if c is ")":pop stack
+# else: add cur
 class Solution:
     def minRemoveToMakeValid(self, s):
+        stack, cur = [], ""
+        for ch in s:
+            if ch == "(":
+                stack.append(cur)
+                cur = ""
+            elif ch == ")":
+                if stack:
+                    cur = stack.pop() + "("+ cur + ")"
+            else :
+                cur += ch
+        while stack:
+            cur = stack.pop() + cur 
+        return cur
+if __name__ == "__main__":
+    sol = Solution()
+    ret = sol.minRemoveToMakeValid("))((")
+    print(ret)
+
+

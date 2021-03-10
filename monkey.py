30452eb1bd3494e098579ddf26e70765be724879
chenhsi <sungchenhsi@gmail.com>
2021-03-10 23:28:45 +0800

    dynamic change in run time

---
+++ monkey.py
@@ -0,0 +1,13 @@
+class A: 
+     def func(self): 
+          print ("func() is being called") 
+def monkey_f(self): 
+     print ("monkey_f() is being called") 
+
+# replacing address of "func" with "monkey_f" 
+A.func = monkey_f 
+obj = A() 
+  
+# calling function "func" whose address got replaced 
+# with function "monkey_f()" 
+obj.func() 
